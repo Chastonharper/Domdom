deca76044a2ebd737f300eb1d02d720251bf81cc
Chaston Harper <chastonharper1@gmail.com>
2020-05-21 14:21:18 -0500

    added button

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+var btn = document.createElement("BUTTON");
+btn.innerHTML = "Add Square";
+document.body.appendChild(btn);
+
+button.addEventListener("click", function () {
+    id++;
+
+    let div = document.createElement("div");
+}
+})
+
