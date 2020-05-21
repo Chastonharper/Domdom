5ffb989951bd0da2a963e508f8a3cfd3fce99e79
Chaston Harper <chastonharper1@gmail.com>
2020-05-21 14:26:09 -0500

    div is added each time button is clicked

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+let id = 0;
 var btn = document.createElement("BUTTON");
 btn.innerHTML = "Add Square";
 document.body.appendChild(btn);
 
-button.addEventListener("click", function () {
+btn.addEventListener("click", function () {
     id++;
 
     let div = document.createElement("div");
-}
-})
+    div.setAttribute("class", "myDiv");
+    div.setAttribute("id", id);
+
+    document.body.appendChild(div);
+});
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+
+});
 
